db4473db1f5f03ac00a8cb2a557dce1bcc8a6685
Keith Yang <yang@keitheis.org>
2013-08-12 12:57:43 +0800

    Added package setup

---
+++ setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+
+import plimsolls
+
+
+setup(
+    name='plimsolls',
+    version='0.1-dev',
+    description="Presentation helpers based on Plim",
+    long_description=(open('README.rst').read() + '\n\n' +
+                      open('CHANGES.rst').read() + '\n\n' +
+                      open('AUTHORS.rst').read()),
+    url='https://github.com/keitheis/plimsolls/',
+    license='MIT',
+    author='Keith Yang',
+    author_email='yang@keitheis.org',
+    packages=find_packages(exclude=['test*']),
+    include_package_data=True,
+    zip_safe=False,
+    platforms='any',
+    install_requires=[
+        'Plim>=0.8.4',
+        'Pygments>=1.4'
+    ],
+    keywords="html presentations slides plim templates",
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Topic :: Text Processing',
+        'Topic :: Multimedia :: Graphics :: Presentation',
+        'Intended Audience :: Developers',
+        'Environment :: Console',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7'
+    ],
+)
